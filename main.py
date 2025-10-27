6c54cd63af4c79e02005a2bb45975af6ed7346e2
ApoloLoxias <apololoxais@gmail.com>
2025-10-27 20:56:10 -0300

    limits fps and cleans the code up a bit

--- main.py
+++ main.py
@@ -6,14 +6,19 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()   #creates a clock objetc
+    dt = 0                                      # will hold time since last tick in miliseconds
+
     while True:                                 # sets infinite loop which keeps game opened
         for event in pygame.event.get():        # makes window close button work
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (34,34,34))
-        pygame.display.flip()                    # updates screen
+        screen.fill((34,34,34))
+        pygame.display.flip()                   # updates screen
+        dt = clock.tick(60)/1000    #caps framerate at 60 fps and returns time since last tick in milisecodns, then converts it to seconds and stores it in dt variable
 
 if __name__ == "__main__":
     main()
