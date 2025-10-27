90c05e333f9ee1469997c082f74051c69b7e7f67
ApoloLoxias <apololoxais@gmail.com>
2025-10-27 20:30:25 -0300

    Implements infinite loop to generate a black screen and keep it open

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+import pygame
+
+from constants import(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:                                 # sets infinite loop which keeps game opened
+        for event in pygame.event.get():        # makes window close button work
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen, (34,34,34))
+        pygame.display.flip()                    # updates screen
 
 if __name__ == "__main__":
     main()
