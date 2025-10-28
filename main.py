a8a4a2d222e056f77a5580249eeb295587cfa79d
ApoloLoxias <apololoxais@gmail.com>
2025-10-27 21:22:28 -0300

    creates player.py and initializes player object on main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pygame
 
-from constants import(SCREEN_WIDTH, SCREEN_HEIGHT)
+from constants import(SCREEN_WIDTH, SCREEN_HEIGHT,
+                      PLAYER_RADIUS)
+from player import Player
 
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((34,34,34))
+        player.draw(screen)
         pygame.display.flip()                   # updates screen
         dt = clock.tick(60)/1000    #caps framerate at 60 fps and returns time since last tick in milisecodns, then converts it to seconds and stores it in dt variable
 
---
+++ player.py
@@ -0,0 +1,21 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_COLOR
+
+class Player(CircleShape):
+    def __init__(self, x, y, PLAYER_RADIUS):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, surface):
+        pygame.draw.polygon(surface, PLAYER_COLOR, self.triangle(), 2)
+        # set surface to screen on main.py when calling this
+        pass
