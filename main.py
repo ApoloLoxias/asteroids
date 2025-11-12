7fd17e0dc812ae6ed67c3f8ccb513a7ee7449f97
ApoloLoxias <apololoxais@gmail.com>
2025-11-12 17:16:05 -0300

    main.py initializes a GUI with a black screen background

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+# Importing libraries
 import pygame
-
+# Importing project modules
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
 
 def main():
+    # Initial terminal message
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    # Preliminary initializations
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # Sets main gameloop
+    while True:
+        log_state() # for boot.dev tests
 
+        screen.fill((34, 34, 34)) # sets black (lightly gray) display surface
+
+        pygame.display.flip() # updates screen at the end of gameloop
 
 if __name__ == "__main__":
     main()
