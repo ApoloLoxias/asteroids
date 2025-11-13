462a0aa8ec89d57523bc99a4506cfd2538154d68
ApoloLoxias <apololoxais@gmail.com>
2025-11-12 23:01:31 -0300

    Makes asteroids splitable and concludes boot.dev course

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,9 @@
 import pygame
+import random
+
 from circleshape import CircleShape
-from constants import LINE_WIDTH, ASTEROID_COLOUR
+from constants import LINE_WIDTH, ASTEROID_COLOUR, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +14,17 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        log_event("asteroid_split")
+        split_angle = random.uniform(20, 50)
+        velocity1 = self.velocity.rotate(split_angle)
+        velocity2 = self.velocity.rotate(-split_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid2 = Asteroid(self.position[0], self.position[1], new_radius)
+        asteroid1.velocity = 1.2 * velocity1
+        asteroid2.velocity = 1.2 * velocity2
